c6268036b3dae086a529a4240c19c32fba5ac5bc
Vs.Lima <200276851+programinggiff@users.noreply.github.com>
2025-04-30 09:40:37 +0000

    commit codar

---
+++ if.else.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main () { 
+    int numero1;
+    int numero2;
+
+    numero1 = 5;
+    numero2 = 10;
+
+    if (numero1 > numero2) { 
+        //Bloco de código a ser executado se a condição for verdadera
+        printf("O número 1 é maior que o número 2\n");    
+    }else {
+        //Bloco de código a ser executado se a condição for falsa
+        printf("O número 1 é menor que o número 2\n");
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ if.else.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+
+int numero = 11;
+if(numero % 2 == 0) {
+    printf("O numero é par\n");
+
+} else {
+    printf("O número é impar\n");
+}
+
+
+}
