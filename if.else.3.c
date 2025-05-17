f4a3ba0853a3cfa618350e361ba0bb56c94ef96a
Vs.Lima <200276851+programinggiff@users.noreply.github.com>
2025-05-17 01:04:19 +0000

    commit_operador_logico

---
+++ if.else.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+
+    int a = 10, b = 5;
+
+    if(a > 0 && b > 0)
+    {
+        printf("os dois numeros são positivos\n");
+    } else {
+        printf("Pelo menos um dos dois números é negativo\n");
+    }
+
+
+
+
+}
+
