a1e1f5b70ed768f25f012c65de4a10f63ea5899a
Vs.Lima <200276851+programinggiff@users.noreply.github.com>
2025-06-08 19:30:17 +0000

    switch_if_else

---
+++ switch.2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+    int dia;
+
+    printf("Entre o valor do dia!\n");
+    scanf("%d", &dia);
+
+    if (dia == 1){
+        printf("Domingo\n");
+    }else if (dia == 2)
+    {
+        printf("Segunda-feria\n");
+    } else if(dia == 3)
+    {
+        printf("Terça-feira\n");
+    } else if(dia == 4)
+    {
+        printf("Quarta-feira\n");
+    } else if (dia == 5)
+    {
+        printf("Quinta-feira\n");
+    } else if(dia == 5)
+    {
+        printf("Sexta-feira\n");
+    } else if (dia == 6)
+    {
+        printf("Sábado\n");
+    } else if(dia == 7) 
+    { 
+        printf("DIA inválido\n");
+    }
+
+    return 0;
+
+}
+
+    
+
+
+
+
+    
+ 
